6a0e9873471ffb2b0d7b35412f6daaa69387ecf2
Sukohi Kuhoh <kuppokeniits1977@yahoo.co.jp>
2013-10-11 15:28:43 +0900

    I changed the method name.

--- RandomNumber.java
+++ RandomNumber.java
@@ -2,7 +2,7 @@ package com.sukohi.lib;
 
 public class RandomNumber {
 
-	public static int getRandom(int min, int max) {   
+	public static int get(int min, int max) {   
   	  
     	return (int) Math.floor(Math.random()*(max-min+1))+min;
         
